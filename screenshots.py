61f0a58c1c97fae28e3122d7433cf3015293f64c
jamesacuan <jec@sacuan.com>
2017-03-20 16:55:20 +0800

    minor fixes

--- screenshots.py
+++ screenshots.py
@@ -13,11 +13,11 @@ month = dt.datetime.now().month
 year  = dt.datetime.now().year
 dirs = os.listdir( assignDrive )
 emptyLogs = [] # list of host without any recent logs
-sT = len(dirs)-1  # files in total
+sT = len(dirs)-1  # dirs in total
 sI = 1  # initial
-sP = 0  # percent
-sD = 0  # delete
-sN = 0  # no logs
+fD = 0  # delete
+fN = 0  # no logs
+fT = 0  # files in total
 
 # map network drive
 sp.call('net use ' +assignDrive + ' /delete')
@@ -28,16 +28,22 @@ def getMonth(val):
     return month[val-1]   
 month = getMonth(month)
 
-def getStatus(pwd, j, k, l, m, n):
+def getStatus(pwd, j, k, m, n, o):
     sp.call('cls', shell=True)
     print("\n" + pwd + "\n")
     print("---------------------------\n")
-    print(j + " of " + k + " host (" + l + "%)")
-    print(m + " outdated logs deleted")
+    print(j + " of " + k + " host (" + str(math.ceil((int(j)/int(k))*100)) + "%)")
     if int(n) < 2:
         print(n + " host has no recent logs.")
     else:
         print(n + " hosts have no recent logs.")
+
+    if int(o) == 0:
+        pT = 0
+    else:
+        pT = math.ceil((int(m)/int(o))*100)
+
+    print(m + " of " + o + " (" + str(pT) + "%) files haves been deleted.")
     print("\n---------------------------\n")
   
 # deletion process
@@ -51,13 +57,13 @@ for pwd in dirs:
     if (pwd != skipdirectory):
         qry = assignDrive + "\\" +pwd
         os.chdir(qry)
-        sP = math.ceil((sI/sT)*100)
-        print(getStatus(pwd, str(sI), str(sT), str(sP), str(sD), str(sN)))
+        print(getStatus(pwd, str(sI), str(sT), str(fD), str(fN), str(fT)))
         if os.listdir(qry) == []:
             emptyLogs.append(pwd)
-            sN = sN+1
+            fN += 1
         
         for file in glob.glob("*"):
+            fT += 1
             if file.find(month)==-1:
                 if file.find(str(year))>0:
                     td = 1
@@ -69,14 +75,11 @@ for pwd in dirs:
                 tD = 1
                 
             if (tD == 1):
+                #print(os.stat(file).st_size) get file size
                 #print(file)
-                os.remove(file)
-                sD = sD+1
+                #os.remove(file)
+                fD += 1
 
         # create dir log of current directory
         sp.call('dir > ' + reportpath + '\\' + pwd + '.txt', shell=True)
-        sI=sI+1
-        
-print(emptyLogs)
-
-    
+        sI += 1
