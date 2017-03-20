85bee43107dfa17b0b40aab30c3792939c944c58
jamesacuan <jec@sacuan.com>
2017-03-20 19:01:33 +0800

    fixes in report and allow redo deletion process

--- screenshots.py
+++ screenshots.py
@@ -11,7 +11,9 @@ assignDrive   = 'T:'
 
 month = dt.datetime.now().month
 year  = dt.datetime.now().year
-dirs = os.listdir( assignDrive )
+dirs = os.listdir(assignDrive)
+redo = 'y'
+red0 = 1
 emptyLogs = [] # list of host without any recent logs
 sT = len(dirs)-1  # dirs in total
 sI = 1  # initial
@@ -28,58 +30,75 @@ def getMonth(val):
     return month[val-1]   
 month = getMonth(month)
 
-def getStatus(pwd, j, k, m, n, o):
+def getStatus(pwd, j, k, m, o):
     sp.call('cls', shell=True)
     print("\n" + pwd + "\n")
     print("---------------------------\n")
     print(j + " of " + k + " host (" + str(math.ceil((int(j)/int(k))*100)) + "%)")
-    if int(n) < 2:
-        print(n + " host has no recent logs.")
-    else:
-        print(n + " hosts have no recent logs.")
-
-    if int(o) == 0:
-        pT = 0
-    else:
-        pT = math.ceil((int(m)/int(o))*100)
-
-    print(m + " of " + o + " (" + str(pT) + "%) files haves been deleted.")
+    print(m + " of " + o + " (" + str(math.ceil(int(o)-int(m))) +" rem.) files haves been deleted.")
     print("\n---------------------------\n")
   
 # deletion process
 sp.call('title Autodelete logs', shell=True)
 sp.call('if not exist ' + reportpath + '\logs mkdir ' + reportpath + '\logs', shell=True) # create report dir
 reportpath = reportpath + '\\logs'
+sp.call('copy /y NUL '+ reportpath +'\\_report.txt >NUL', shell=True)
 
-for pwd in dirs:
-    
-    tD = 0 # to be deleted?
-    if (pwd != skipdirectory):
-        qry = assignDrive + "\\" +pwd
-        os.chdir(qry)
-        print(getStatus(pwd, str(sI), str(sT), str(fD), str(fN), str(fT)))
-        if os.listdir(qry) == []:
-            emptyLogs.append(pwd)
-            fN += 1
-        
-        for file in glob.glob("*"):
-            fT += 1
-            if file.find(month)==-1:
-                if file.find(str(year))>0:
-                    td = 1
+while(red0==1):
+    for pwd in dirs:        
+        tD = 0 # to be deleted?
+        if (pwd != skipdirectory):
+            qry = assignDrive + "\\" +pwd
+            os.chdir(qry)
+            print(getStatus(pwd, str(sI), str(sT), str(fD), str(fT)))
+            if fN == 0:
+                print('')
+            elif fN < 2:
+                print(str(fN) + " host has no recent logs:")
+                print(emptyLogs)
+            else:
+                print(str(fN) + " hosts have no recent logs.")
+                print(emptyLogs)
+                
+            if os.listdir(qry) == []:
+                sp.call('echo ' + pwd + '>>"'+reportpath + '\\_report.txt"', shell=True)
+                emptyLogs.append(pwd)
+                fN += 1
+            
+            for file in glob.glob("*"):
+                i = 1
+                fT += 1
+                if file.find(month)==-1:
+                    if file.find(str(year))>0:
+                        td = 1
+                        
+                if file.find("html")>0:
+                    tD = 1
+                
+                while(i<=9):
+                    if file.find(".00"+str(i))>0:
+                        tD = 1
+                    i+=1
                     
-            if file.find("html")>0:
-                tD = 1
-                
-            if file.find(".009")>0:               
-                tD = 1
-                
-            if (tD == 1):
-                #print(os.stat(file).st_size) get file size
-                #print(file)
-                #os.remove(file)
-                fD += 1
+                if (tD == 1):
+                    # print(os.stat(file).st_size) get file size
+                    # print(file)
+                    os.remove(file)
+                    fD += 1
 
-        # create dir log of current directory
-        sp.call('dir > ' + reportpath + '\\' + pwd + '.txt', shell=True)
-        sI += 1
+            # create dir log of current directory
+            # sp.call('dir > ' + reportpath + '\\' + pwd + '.txt', shell=True)
+            
+            sI += 1
+
+    
+    # rescan
+    sp.call('cls', shell=True)
+    redo = input("Do you wish to rescan the logs folder? [y/n]: ")
+    if redo in ['y','Y','yes','YES','Yes']:
+        red0=1
+    else: red0=0
+    
+sp.call('cls', shell=True)    
+print("Finished scanning log. check this directory for info:")
+sp.call('echo '+reportpath, shell=True)
